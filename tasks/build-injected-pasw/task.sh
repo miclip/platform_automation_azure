bcce579f617079f9dd49c7b5d69aca39e834fa7e
Michael Lipscombe <mlipscombe@outlook.com>
2019-01-04 20:04:20 -0700

    download winfs injector

--- tasks/build-injected-pasw/task.sh
+++ tasks/build-injected-pasw/task.sh
@@ -4,15 +4,15 @@ set -eu
 echo "Entering Build Injected PASW Task"
 
 #Unzip the WinFS Injector
-cd pivnet-winfs-injector
+cd winfs-injector
 unzip "*winfs*"
 chmod 700 winfs-injector-linux
 
 #Find the original tile
-cd ../pivnet-pasw
+cd ../downloaded-product
 FILE_PATH=`find . -name *.pivotal | sort | head -1`
 
 
 #Run the injector with the input as the original tile and output the winfs-injected tile
-cd ../pivnet-winfs-injector
-./winfs-injector-linux --input-tile ../pivnet-pasw/$FILE_PATH --output-tile ../pasw-injected/$FILE_PATH
+cd ../winfs-injector
+./winfs-injector-linux --input-tile ../downloaded-product/$FILE_PATH --output-tile ../pasw-injected/$FILE_PATH
